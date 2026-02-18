4075ba853885eaa410ac699c10744d06f5c0ad37
Azizur Rahman <azizur.rahman@orbitax.com>
2026-02-18 19:01:47 +0600

    change ui design

--- Frontend/Dashboard/src/app/Shared/Components/sidebar-component/sidebar-component.ts
+++ Frontend/Dashboard/src/app/Shared/Components/sidebar-component/sidebar-component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import { CourseService } from 'src/app/services/course.service';
 
 @Component({
   selector: 'app-sidebar-component',
-  imports: [],
   templateUrl: './sidebar-component.html',
-  styleUrl: './sidebar-component.scss',
+  styleUrls: ['./sidebar-component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  courseData: any;
+  courseId: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    // private courseService: CourseService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.courseId = params['id'];
+      this.loadCourseData();
+    });
+  }
+
+  loadCourseData() {
+    // this.courseService.getCourseStructure(this.courseId).subscribe(data => {
+    //   this.courseData = data;
+    // });
+  }
 }
