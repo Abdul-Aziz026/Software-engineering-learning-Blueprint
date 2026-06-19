efe1b03a1e0486dc939dc00627087a60b8214e16
azizurrahman2 <azizur.rahman@orbitax.com>
2026-06-19 17:04:11 +0600

    update base entity class

--- Backend/Domain/Entities/BaseEntity.cs
+++ Backend/Domain/Entities/BaseEntity.cs
@@ -5,6 +5,26 @@ namespace Domain.Entities;
 public abstract class BaseEntity
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
-    //public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    //public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not BaseEntity other)
+        {
+            return false;
+        }
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+        if (GetType() != other.GetType())
+        {
+            return false;
+        }
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
